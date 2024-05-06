9db8d678f2c67c34571de85f4547d4313e8e07bb
Maria Clara <mariaclariagomes@gmail.com>
2024-05-06 08:47:10 -0300

    cmmit

---
+++ codigo/index2.js
@@ -0,0 +1,9 @@
+const categoria = document.querySelectorAll("categorySelector");
+console.log(categoria);
+
+const nav = document.createElement("nav");
+document.querySelector("body").appendChild(nav);
+
+const body = document.querySelector
+
+body.insertBefore(nav,main);
