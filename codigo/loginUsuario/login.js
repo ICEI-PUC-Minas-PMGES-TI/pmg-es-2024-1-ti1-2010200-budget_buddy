f76567a886923f0872e350f747d833340bf7fda6
debora67 <144275722+debora67@users.noreply.github.com>
2024-05-15 16:00:41 -0300

    Update login.js

--- codigo/loginUsuario/login.js
+++ codigo/loginUsuario/login.js
@@ -5,21 +5,18 @@ document.addEventListener("DOMContentLoaded", function() {
             event.preventDefault();
             var username = document.getElementById("username").value;
             var senha = document.getElementById("senha").value;
-
-            // Recuperar dados de cadastro armazenados no localStorage
+            
             var cadastroData = localStorage.getItem("cadastroData");
             if (cadastroData) {
                 cadastroData = JSON.parse(cadastroData);
-                if (cadastroData.email === username || cadastroData.nomeUsuario === username) {
+                if (cadastroData.email === username) {
                     if (cadastroData.senha === senha) {
                         alert("Login bem-sucedido!");
-                        // Aqui você pode redirecionar o usuário para a página inicial, por exemplo
-                        window.location.href = "pagina_inicial.html";
                     } else {
                         alert("Senha incorreta. Por favor, tente novamente.");
                     }
                 } else {
-                    alert("Nome de usuário/e-mail não encontrado. Por favor, registre-se primeiro.");
+                    alert("E-mail não encontrado.");
                 }
             } else {
                 alert("Não há cadastros armazenados. Por favor, registre-se primeiro.");
